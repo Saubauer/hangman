0064cbe2b98675b5b6c516fb46c9119f941b81c0
Saubauer <Saubauer@gmx.net>
2021-06-17 20:25:14 +0200

    some fixes

--- lib/classes.rb
+++ lib/classes.rb
@@ -1,183 +1,190 @@
 #################################################################
 class Display
-	def self.intro
-		cls
-		puts "Hangman Game
+  def self.intro
+    cls
+    puts "Hangman Game
 		"
-		puts "Computer chooses a random word from a dictonary "
-		puts "and you have 8 changes to guess the word correctly
+    puts 'Computer chooses a random word from a dictonary '
+    puts "and you have 8 changes to guess the word correctly
 		"
-		puts "(Press any key to continue)"
-		gets 
-		cls
-	end
+    puts '(Press any key to continue)'
+    gets
+    cls
+  end
 
-	def self.goodbye
-		cls
-		puts "Thanks for playing Hangman"
-		sleep(2)
-		cls
-	end
-	def self.guess(guess, word, wrong=[], mistake)
-		cls
-		puts "You guessed: '#{guess.upcase}'
-		" unless guess == "";
-		puts "Word: #{word.join("")}
-		"
-		puts "Wrong guesses: #{wrong.join("")}" unless wrong.empty?
-		puts "and mistakes: #{mistake} / 8
-		" unless mistake == 0
-	end
+  def self.goodbye
+    cls
+    puts 'Thanks for playing Hangman'
+    sleep(2)
+    cls
+  end
 
-	def self.game_end(win, word)
-		puts ""
-		puts "You won!" if win == 1
-		puts "You lose!" if win == 0
-		puts ""
-		puts "Word was: #{word.join("")}"
-		gets
-	end
+  def self.guess(guess, word, wrong = [], mistake)
+    cls
+    unless guess == ''
+      puts "You guessed: '#{guess.upcase}'
+    "
+    end
+    puts "Word: #{word.join('')}
+		"
+    puts "Wrong guesses: #{wrong.join('')}" unless wrong.empty?
+    unless mistake == 0
+      puts "and mistakes: #{mistake} / 8
+    "
+    end
+  end
+
+  def self.game_end(win, word)
+    puts ''
+    puts 'You won!' if win == 1
+    puts 'You lose!' if win == 0
+    puts ''
+    puts "Word was: #{word.join('')}"
+    gets
+  end
 end
+
 #################################################################
 class Game
-	def initialize(mistakes=0,correctWord=0,wrongGuesses=[],guessWord=[])
-		dict_cleaning("5desk.txt") unless File.exists?("wordlist.txt")
-		if correctWord == 0 then
-			@correctWord = random_word.split("") 
-		else
-			@correctWord = correctWord
-		end
-		if guessWord.empty? then
-			@guessWord = Array.new(@correctWord.length,"_")
-		else
-			@guessWord = guessWord
-		end	
-		@wrongGuesses = wrongGuesses
-		@mistakes = mistakes
-	end
+  def initialize(mistakes = 0, correctWord = 0, wrongGuesses = [], guessWord = [])
+    dict_cleaning('5desk.txt') unless File.exist?('wordlist.txt')
+    @correctWord = if correctWord == 0
+                     random_word.split('')
+                   else
+                     correctWord
+                   end
+    @guessWord = if guessWord.empty?
+                   Array.new(@correctWord.length, '_')
+                 else
+                   guessWord
+                 end
+    @wrongGuesses = wrongGuesses
+    @mistakes = mistakes
+  end
 
-	def random_word
-		File.open("wordlist.txt") do |file|
-			file.readlines.sample.gsub!("\n","")
-		end
-	end
+  def random_word
+    File.open('wordlist.txt') do |file|
+      file.readlines.sample.gsub!("\n", '')
+    end
+  end
 
-	def new_game
-		puts "Word: #{@guessWord.join("")}
+  def new_game
+    puts "Word: #{@guessWord.join('')}
 		"
-		guessing
-	end
+    guessing
+  end
 
-	def guessing 
-		until @mistakes == 8 || @guessWord == @correctWord do 
-			guess = make_guess
-			return game_save if guess == "save"
-			check_guess(guess)
-			Display.guess(guess,@guessWord,@wrongGuesses,@mistakes)
-		end
+  def guessing
+    until @mistakes == 8 || @guessWord == @correctWord
+      guess = make_guess
+      return game_save if guess == 'save'
 
-		if @mistakes == 8 then
-			Display.game_end(0, @correctWord)
-			$main.start
-		else
-			Display.game_end(1, @correctWord)
-			$main.start
-		end
-	end
+      check_guess(guess)
+      Display.guess(guess, @guessWord, @wrongGuesses, @mistakes)
+    end
 
-	def make_guess
-		puts "Enter single letter or 'save'"
-		input = gets.chomp
+    if @mistakes == 8
+      Display.game_end(0, @correctWord)
+    else
+      Display.game_end(1, @correctWord)
+    end
+    $main.start
+  end
 
-		return input if input == "save"
-		return input if input.length == 1 && input.match?(/[a-z]/) && @guessWord.none?(input) && @wrongGuesses.none?(input)
-		puts "Either you chose a letter you already had before or entered an invalid input"
-		make_guess
+  def make_guess
+    puts "Enter single letter or 'save'"
+    input = gets.chomp
 
-	end
+    return input if input == 'save'
+    return input if input.length == 1 && input.match?(/[a-z]/) && @guessWord.none?(input) && @wrongGuesses.none?(input)
 
-	def check_guess(guess)
-		if @correctWord.include?(guess) then
-			@correctWord.each_with_index {|letter,index|
-				@guessWord[index] = letter if letter == guess
-			}
-		else
-			@mistakes += 1
-			@wrongGuesses.push(guess)
-		end
-	end
+    puts 'Either you chose a letter you already had before or entered an invalid input'
+    make_guess
+  end
 
-	def game_save
-		Dir.mkdir('sav') unless Dir.exist?('sav')
-		save = JSON::dump({
-	      	:mistakes => @mistakes,
-	      	:correctWord => @correctWord,
-	      	:wrongGuesses => @wrongGuesses,
-	      	:guessWord => @guessWord
-    	})
-		puts "Enter save name"
-		savename = gets.chomp
+  def check_guess(guess)
+    if @correctWord.include?(guess)
+      @correctWord.each_with_index do |letter, index|
+        @guessWord[index] = letter if letter == guess
+      end
+    else
+      @mistakes += 1
+      @wrongGuesses.push(guess)
+    end
+  end
 
-		filename = "sav/#{savename}.json"
-		File.open(filename, 'w') do |file|
-			file.puts save
-		end
-		$main.start
-	end
+  def game_save
+    Dir.mkdir('sav') unless Dir.exist?('sav')
+    save = JSON.dump({
+                       mistakes: @mistakes,
+                       correctWord: @correctWord,
+                       wrongGuesses: @wrongGuesses,
+                       guessWord: @guessWord
+                     })
+    puts 'Enter save name'
+    savename = gets.chomp
 
-	def game_load
-		Display.guess("",@guessWord,@wrongGuesses,@mistakes)
-		new_game
-	end
+    filename = "sav/#{savename}.json"
+    File.open(filename, 'w') do |file|
+      file.puts save
+    end
+    $main.start
+  end
+
+  def game_load
+    Display.guess('', @guessWord, @wrongGuesses, @mistakes)
+    new_game
+  end
 end
+
 #################################################################
 class Main
-	def initialize
-		@game = nil 
-	end
+  def initialize
+    @game = nil
+  end
 
-	def start
-		cls
-		puts "Do you want to start a new game, load a old game or quit?
+  def start
+    cls
+    puts "Do you want to start a new game, load a old game or quit?
 		"
-		puts "Enter either 'n' or 'new' for new game 'load' or 'l' for load or anything else to quit"
+    puts "Enter either 'n' or 'new' for new game 'load' or 'l' for load or anything else to quit"
 
-		input = gets.chomp
+    input = gets.chomp
 
-		if input.downcase == "n" || input.downcase == "new" then 
-			new_game
-		elsif input.downcase == "load" || input.downcase == "l" then
-			game_load
-		else
-			Display.goodbye
-		end
-	end
+    if input.downcase == 'n' || input.downcase == 'new'
+      new_game
+    elsif input.downcase == 'load' || input.downcase == 'l'
+      game_load
+    else
+      Display.goodbye
+    end
+  end
 
-	def new_game
-		@game = Game.new
-		Display.intro
-		@game.new_game
-	end
+  def new_game
+    @game = Game.new
+    Display.intro
+    @game.new_game
+  end
 
-	def game_load
-		cls
-		@save = nil
-		puts "Enter save name"
-		savename = gets.chomp
+  def game_load
+    cls
+    @save = nil
+    puts 'Enter save name'
+    savename = gets.chomp
 
-		filename = "sav/#{savename}.json"
-		if File.exist?(filename) then
-		File.open(filename, 'r') do |file|
-			@save = file.read
-		end
-		else
-			puts "File doesn't exist"
-			sleep(1)
-			start
-		end
-		
-		data = JSON.load @save
-		@game = Game.new(data['mistakes'], data['correctWord'], data['wrongGuesses'], data['guessWord'])
-		@game.game_load
-	end
+    filename = "sav/#{savename}.json"
+    if File.exist?(filename)
+      File.open(filename, 'r') do |file|
+        @save = file.read
+      end
+    else
+      puts "File doesn't exist"
+      sleep(1)
+      start
+    end
+
+    data = JSON.load @save
+    @game = Game.new(data['mistakes'], data['correctWord'], data['wrongGuesses'], data['guessWord'])
+    @game.game_load
+  end
 end
--- lib/modules.rb
+++ lib/modules.rb
@@ -1,21 +1,22 @@
 module Basic
-	def dict_cleaning(filename)
-		return "Error" unless File.exist?(filename)
-		dict = File.open(filename)
-		wordlist = []
+  def dict_cleaning(filename)
+    return 'Error' unless File.exist?(filename)
 
-		dict.each do |line|
-		 line.gsub!("\n","")
-		 wordlist.push(line.downcase) if line.length > 4 && line.length < 13
-		end
+    dict = File.open(filename)
+    wordlist = []
 
-		File.open("wordlist.txt","w") do |file|
-			file.write(wordlist.join("\n"))
-		end
-		dict.close
-	end
+    dict.each do |line|
+      line.gsub!("\n", '')
+      wordlist.push(line.downcase) if line.length > 4 && line.length < 13
+    end
 
-	def cls
-		system('cls') || system('clear')
-	end
+    File.open('wordlist.txt', 'w') do |file|
+      file.write(wordlist.join("\n"))
+    end
+    dict.close
+  end
+
+  def cls
+    system('cls') || system('clear')
+  end
 end
